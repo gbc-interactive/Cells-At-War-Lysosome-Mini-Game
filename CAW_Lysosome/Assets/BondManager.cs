1da8fe33c195b6e4399daa5ebb210a22a8227b74
ayden.heyink2112@gmail.com <91908065+AydenHeyink@users.noreply.github.com>
2023-10-30 19:16:11 -0400

    many managers started
    Started the bond manager, which will be responsible for spawning random amino acids. This, along with the bonds and their bridges

---
+++ CAW_Lysosome/Assets/BondManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class BondManager : MonoBehaviour
+{
+    [SerializeField] Sprite[] aa;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
