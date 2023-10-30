5d9a2fc5c181e990e4a83e11b3222e01a938e602
ayden.heyink2112@gmail.com <91908065+AydenHeyink@users.noreply.github.com>
2023-10-30 18:55:34 -0400

    simple timer created

---
+++ CAW_Lysosome/Assets/Scripts/TimerScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+    [SerializeField] float TimeLeft;
+    [SerializeField] bool TimerOn = false;
+
+    public Text timerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimerOn = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TimerOn)
+        {
+            if (TimeLeft > 0)
+            {
+                TimeLeft -= Time.deltaTime;
+                UpdateTimer(TimeLeft);
+            }
+        }
+    }
+
+    void UpdateTimer(float currentTime)
+    {
+        currentTime += 1;
+
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+
+        timerText.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+    }
+}
