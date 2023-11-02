0005a3c3ed24926a08ee4716cc279cf058d846d2
ayden.heyink2112@gmail.com <91908065+AydenHeyink@users.noreply.github.com>
2023-11-01 22:17:01 -0400

    Begun to work on amino acid station
    Starting to work on stations, which Im calling each of the bonds that the player stops at to rapid button press

--- CAW_Lysosome/Assets/Scripts/Player.cs
+++ CAW_Lysosome/Assets/Scripts/Player.cs
@@ -1,22 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Common;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] float playerSpeed = 3f;
-
+    bool isAtPressStation;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isAtPressStation= false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float x = transform.position.x;
+        if (!isAtPressStation)
+        {
+            MoveRight();
+        }
+        else if (isAtPressStation) 
+        {
+            Debug.Log("At station");
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                isAtPressStation= true;
+            }
+        }
+    }
 
-        transform.position = new Vector3(x += playerSpeed * Time.deltaTime ,-2,0);
+    private void MoveRight()
+    {
+        float x = transform.position.x;
+        transform.position = new Vector3(x += playerSpeed * Time.deltaTime, -2, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Station")
+        {
+            isAtPressStation = true;
+        }
     }
 }
